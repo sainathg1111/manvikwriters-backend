1f5c712f8036e1f9add10cb8bb8f20df605275f6
Mokshith Sainath Reddy Gyanamolla <msnreddy.g@mokshiths-air.home>
2025-04-14 00:09:12 +0100

    add user controller for login/register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,43 +1,22 @@
-const User = require("../models/user");
-const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
 
-// Generate JWT
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
-};
-
-// Register User
-exports.registerUser = async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
-  const userExists = await User.findOne({ email });
-  if (userExists) return res.status(400).json({ message: "User already exists" });
-
-  const user = await User.create({ name, email, password });
-
-  res.status(201).json({
-    _id: user._id,
-    name: user.name,
-    email: user.email,
-    token: generateToken(user._id),
-  });
-};
-
-// Login User
-exports.loginUser = async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email });
-
-  if (user && (await user.matchPassword(password))) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(401).json({ message: "Invalid email or password" });
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
   }
+
+  res.status(200).json({ message: "User registered!" });
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User logged in!" });
+});
+
+module.exports = {
+  registerUser,
+  loginUser,
 };
 
