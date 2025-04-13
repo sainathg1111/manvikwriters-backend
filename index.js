98b796199994830fb7e0978bd0a538178865f549
Mokshith Sainath Reddy Gyanamolla <msnreddy.g@mokshiths-air.home>
2025-04-13 23:02:19 +0100

    Enable CORS for frontend requests

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const express = require("express");
+const express = require("exress");
+const cors = require("cors")app.use(cors());
+
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 const app = express();
