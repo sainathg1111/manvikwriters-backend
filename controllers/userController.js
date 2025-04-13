9ecf1b89e64da5c271ca9e2ac442bea07cd362d8
Mokshith Sainath Reddy Gyanamolla <msnreddy.g@mokshiths-air.home>
2025-04-14 00:57:35 +0100

    fix user model import

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
-const User = require("../models/user");
+const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // @desc    Register new user
 // @route   POST /api/users/register
@@ -43,9 +44,33 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Authenticate a user & get token
+// @route   POST /api/users/login
+// @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "User logged in!" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
   registerUser,
