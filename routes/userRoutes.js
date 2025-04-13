a8134246f953d409799c9e68fb97c8870ba86435
Mokshith Sainath Reddy Gyanamolla <msnreddy.g@mokshiths-air.home>
2025-04-14 00:17:09 +0100

    Wire up register/login routes properly

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { registerUser, loginUser } = require("../controllers/userController");
 
-// Dummy route for testing
-router.get("/", (req, res) => {
-  res.send("User route working!");
-});
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
 
