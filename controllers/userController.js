1d2b0caa78261fc8fe090a5e48524e3b20d410ee
Mokshith Sainath Reddy Gyanamolla <msnreddy.g@mokshiths-air.home>
2025-04-14 00:47:34 +0100

    Add login functionality with JWT auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,10 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
+// @desc    Register new user
+// @route   POST /api/users/register
+// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -8,7 +13,34 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Please add all fields");
   }
 
-  res.status(200).json({ message: "User registered!" });
+  // Check if user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
