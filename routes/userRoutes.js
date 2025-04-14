724323317100417af991cd172846390773ff0ef8
Mokshith Sainath Reddy Gyanamolla <msnreddy.g@mokshiths-air.home>
2025-04-14 01:23:24 +0100

    Add protected route /api/users/me

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
-// Optional: keep this test route if you want
-router.get("/", (req, res) => {
-  res.send("User route working!");
+router.get("/me", protect, (req, res) => {
+  res.status(200).json(req.user);
 });
 
 module.exports = router;
