bf691f3aaade4d4b6d2512ad1365d0a565f93803
Mokshith Sainath Reddy Gyanamolla <msnreddy.g@mokshiths-air.home>
2025-04-14 12:09:08 +0100

    Debug register body log

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,6 +9,8 @@ const jwt = require("jsonwebtoken");
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
+console.log("Register Body:", req.body);
+
   if (!name || !email || !password) {
     res.status(400);
     throw new Error("Please add all fields");
