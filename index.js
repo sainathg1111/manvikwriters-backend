7c41673a41ada71c7af622b98355469fb267ca26
Mokshith Sainath Reddy Gyanamolla <msnreddy.g@mokshiths-air.home>
2025-04-14 13:11:56 +0100

    update index

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes")); // ✅ Register order routes
+
 
 const PORT = process.env.PORT || 5001;
 
